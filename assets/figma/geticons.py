1f72b720e6bc73b36c8075e72c86771b7d8540cc
Dominik Ludera <domludera@gmail.com>
2020-02-13 21:52:45 -0500

    wireframes added

--- assets/figma/geticons.py
+++ assets/figma/geticons.py
@@ -3,16 +3,16 @@ import os
 
 simpleicons = []
 
-for root, dirs, files in os.walk("icons", topdown = False):
+for root, dirs, files in os.walk("wireframes", topdown = False):
     for name in files:
-        if re.match("\w+-outline\.svg", name):
+        if re.match("\S+.png", name):
             simpleicons.append(name)
 
 
-"""
 for i in simpleicons:
-    print("<li><img class=\"icons\" src=\"assets/simpleicons/%s\"/></li>" % i)
+    print("<img class=\"wireframe\" src=\"assets/figma/wireframes/%s\"/>" % i)
 
 """
 for i in simpleicons:
     os.system("cp icons/%s simpleicons/" % i)
+"""
